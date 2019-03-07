ac72892bf041055ab5af516342dea7d7db39ba2b
Kazuya Shimura <g17tk008@yamanashi.ac.jp>
2019-03-07 20:57:37 +0900

    [update] プログラムにコメント追加. READMEにpdbの使い方を記載.

--- program/sentence_reader.py
+++ program/sentence_reader.py
@@ -7,12 +7,16 @@ from itertools import chain
 
 import lxml.etree as et
 import numpy as np
-from context2vec.common.defs import Toks
 from nltk.tokenize import sent_tokenize, word_tokenize
 from tqdm import tqdm
 
 MAX_SENTENCE_LEN = 10
 
+"""
+データを数値に変換する関数
+<PAD>: Padding文字
+<UNK>: 未知語
+"""
 def convert_numeric_data(data, batchsize, word2index,max_len):
     converted_data = {"indexed_text":[], "labels":[],"positions":[], "keys":[], "doc_category":[]}
 
@@ -44,6 +48,9 @@ def convert_numeric_data(data, batchsize, word2index,max_len):
     
     return converted_data
 
+"""
+1文書を1行に変換する関数
+"""
 def convert_sentences(sentences_dic, max_sentence_len=MAX_SENTENCE_LEN):
 
     joint_context = []
@@ -68,6 +75,16 @@ def convert_sentences(sentences_dic, max_sentence_len=MAX_SENTENCE_LEN):
     return doc_dic, len(joint_context[0])
 
 
+"""
+XMLファイルを読み込む関数
+<PAD>: Padding文字
+pos: 品詞
+lemma: 原形
+context: 本文
+instanceが語義解消単語
+wfがその他の単語
+goldAnsが正解の語義
+"""
 def load_xml(path):
 
     parser = et.XMLParser()
@@ -118,6 +135,10 @@ def load_xml(path):
 
     return texts_dic, word_freq, max_one_sentence_len
 
+
+"""
+データを管理するクラス
+"""
 class SentenceReaderDir(object):
 
     def __init__(self, data_path, batchsize):
@@ -128,15 +149,17 @@ class SentenceReaderDir(object):
         self.trimmed_word2count, self.word2index, self.index2word = self.read_and_trim_vocab()
         self.total_words = sum(self.trimmed_word2count.values())
         self.catgy = defaultdict( lambda: len(self.catgy) ) ##分類カテゴリ名とカテゴリ番号のマッピング
-        self.key2sid = {} ## key(activate.v)とsense_id(カテゴリ名)のマッピング
-        self.key2netout = {} ## keyとネットワークの出力層のユニット番号の対応
-        self.senseid2netout = {}
+        self.key2sid = {} ## key(単語の原形)とsense_id(語義)のマッピング
+        self.key2netout = {} ## key(単語の原形)とネットワークの出力層のユニット番号の対応
+        self.senseid2netout = {} ## sense_id(語義)とネットワークの出力層のユニット番号の対応
         self.doc_catgy = defaultdict( lambda: len(self.doc_catgy) ) ##文書分類用のカテゴリ
-        self.make_catgy()
+        self.make_catgy() ## 語義カテゴリを作成
         self.make_key2netout()
         self.add_wsdtag_to_vocab()
 
-
+    """
+    語義カテゴリ全体を読み込む関数
+    """
     def make_catgy(self):
 
         for doc in tqdm(sorted(self.data.items(),key=lambda x: x[0])):
@@ -163,12 +186,9 @@ class SentenceReaderDir(object):
                 pdb.set_trace()
             [self.catgy[sid] for sid in sents['answers'] if sid != "<PAD>"]
 
-        if len(self.doc_catgy) != 3:
-            all_labels = ["GENT","GSCI","GPOL"]
-            for l in all_labels:
-                self.doc_catgy[l]
-
-
+    """
+    語義と出力層のユニット番号の対応を作る関数
+    """
     def make_key2netout(self):
         for k,v in self.key2sid.items():
             self.key2netout[k] = [self.catgy[_v] for _v in v]
@@ -176,29 +196,31 @@ class SentenceReaderDir(object):
                 self.senseid2netout[_v] = self.key2netout[k]
 
 
-
+    """
+    語彙を読み込む関数
+    <UNK>: 未知語
+    未知語の処理は今回はない.
+    """
     def read_and_trim_vocab(self, trimfreq=0):
         word2count = self.word_freq
         trimmed_word2count = collections.Counter()
-        index2word = {Toks.UNK:'<UNK>', Toks.BOS:'<BOS>', Toks.EOS:'<EOS>'}
-        word2index = {'<UNK>':Toks.UNK, '<BOS>':Toks.BOS, '<EOS>':Toks.EOS}
+        index2word = {0:'<UNK>'}
+        word2index = {'<UNK>': 0}
         unknown_counts = 0
-
+        pdb.set_trace()
         for word, count in sorted(word2count.items()):
-            if count >= trimfreq and word.lower() != '<unk>' and word.lower() != '<rw>':    
-                ind = len(word2index)
-                word2index[word] = ind
-                index2word[ind] = word
-                trimmed_word2count[ind] = count
-            else:
-                unknown_counts += count
-        trimmed_word2count[word2index['<BOS>']] = 0
-        trimmed_word2count[word2index['<EOS>']] = 0
+            ind = len(word2index)
+            word2index[word] = ind
+            index2word[ind] = word
+            trimmed_word2count[ind] = count
+
         trimmed_word2count[word2index['<UNK>']] = unknown_counts
 
-
         return trimmed_word2count, word2index, index2word
-
+    
+    """
+    語義を語彙に追加する関数.
+    """
     def add_wsdtag_to_vocab(self):
         for wsdtag in self.catgy.keys():
             ind = len(self.word2index)
